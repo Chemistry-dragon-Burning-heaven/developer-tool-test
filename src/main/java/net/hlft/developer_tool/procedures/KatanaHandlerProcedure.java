1f5839106d40d4bf0a412c54cf9f830d00e9fa01
Lzx783626185@163.com <Lzx783626185@163.com>
2021-06-19 10:31:46 +0800

    0.0.3武士刀-速度buff

---
+++ src/main/java/net/hlft/developer_tool/procedures/KatanaHandlerProcedure.java
@@ -0,0 +1,61 @@
+package net.hlft.developer_tool.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.hlft.developer_tool.DeveloperToolModElements;
+import net.hlft.developer_tool.DeveloperToolMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@DeveloperToolModElements.ModElement.Tag
+public class KatanaHandlerProcedure extends DeveloperToolModElements.ModElement {
+	public KatanaHandlerProcedure(DeveloperToolModElements instance) {
+		super(instance, 11);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				DeveloperToolMod.LOGGER.warn("Failed to load dependency entity for procedure KatanaHandler!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if ((ItemTags.getCollection().getTagByID(new ResourceLocation(("developer_tool:katana").toLowerCase(java.util.Locale.ENGLISH)))
+				.contains(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 60, (int) 0, (false), (false)));
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			Entity entity = event.player;
+			World world = entity.world;
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
